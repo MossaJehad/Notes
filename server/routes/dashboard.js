159a8656c9ef98aafb5ba78635b8ceed20041abd
MossaJehad <Moosa.Jehad65@gmail.com>
2024-09-08 15:35:34 +0300

    update notes

--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -4,5 +4,7 @@ const dashController = require('../controllers/dashController')
 const { isLoggedIn } = require('../middleware/checkAuth')
 
 router.get('/dashboard', isLoggedIn, dashController.dashboard)
+router.get('/dashboard/item/:id', isLoggedIn, dashController.dashboardViewNote)
+router.put('/dashboard/item/:id', isLoggedIn, dashController.dashboardUpdateNote)
 
 module.exports = router
