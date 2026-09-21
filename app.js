9d6a4acf45458291882b6319f61c02a77013015c
Mossa Jehad <m.jehad@tameed.sa>
2026-09-21 13:10:55 +0300

    feat(ui): highlight active nav tab on URL entry and add sticky footer with page links

--- app.js
+++ app.js
@@ -45,6 +45,11 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('layout', './layouts/main')
 app.set('view engine', 'ejs')
 
+app.use((req, res, next) => {
+    res.locals.currentRoute = req.path.replace(/\/$/, '') || '/';
+    next();
+})
+
 //Routes
 app.use('/', require('./server/routes/auth'))
 
