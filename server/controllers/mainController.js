1bd24a2fa1ee483f2fc64b0ffc2554786f61e4ea
Mossa Jehad <m.jehad@tameed.sa>
2026-09-21 12:41:07 +0300

    feat(all): add features, faqs, and expanded about pages with responsive layouts and navigation links

--- server/controllers/mainController.js
+++ server/controllers/mainController.js
@@ -11,8 +11,24 @@ exports.homepage = async (req, res) => {
 
 exports.about = async (req, res) => {
     const locals = {
-        title: 'About NodeJS Notes',
-        description: 'Simple Notes App created with NodeJS, Express and MongoDB.'
+        title: 'About - NodeJS Notes',
+        description: 'Learn more about the Notes application.'
     }
-    res.render('about', locals)
+    res.render('about', { locals })
+}
+
+exports.features = async (req, res) => {
+    const locals = {
+        title: 'Features - NodeJS Notes',
+        description: 'Explore key features of Notes app.'
+    }
+    res.render('features', { locals })
+}
+
+exports.faq = async (req, res) => {
+    const locals = {
+        title: 'FAQs - NodeJS Notes',
+        description: 'Frequently Asked Questions about Notes app.'
+    }
+    res.render('faq', { locals })
 }
--- server/routes/index.js
+++ server/routes/index.js
@@ -4,5 +4,8 @@ const mainController = require('../controllers/mainController')
 
 router.get('/', mainController.homepage)
 router.get('/about', mainController.about)
+router.get('/features', mainController.features)
+router.get('/faq', mainController.faq)
+router.get('/faqs', (req, res) => res.redirect('/faq'))
 
 module.exports = router
